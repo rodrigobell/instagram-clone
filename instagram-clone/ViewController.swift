a9eb2aef383ed40f3d5d2c8e818b7defe2deff92
rodrigobell <rbell594@gmail.com>
2017-03-05 18:10:57 -0800

    Quickstart video completed

--- instagram-clone/ViewController.swift
+++ instagram-clone/ViewController.swift
@@ -7,8 +7,11 @@
 //
 
 import UIKit
+import Parse
 
 class ViewController: UIViewController {
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +23,32 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onSignIn(_ sender: Any) {
+        PFUser.logInWithUsername(inBackground: usernameTextField.text!, password: passwordTextField.text!) { (user: PFUser?, error: Error?) in
+            if user != nil {
+                print("User log in successful")
+                self.performSegue(withIdentifier: "login-segue", sender: nil)
+            } else {
+                print("Error: \(error?.localizedDescription)")
+            }
+        }
+    }
 
+    @IBAction func onSignUp(_ sender: Any) {
+        let newUser = PFUser()
+        
+        newUser.username = usernameTextField.text
+        newUser.password = passwordTextField.text
+        
+        newUser.signUpInBackground { (success: Bool, error: Error?) in
+            if success {
+                print("User sign up successful")
+                self.performSegue(withIdentifier: "login-segue", sender: nil)
+            } else {
+                print("Error: \(error?.localizedDescription)")
+            }
+        }
+    }
+    
 }
 
